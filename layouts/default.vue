08bfbca7ecac0e8afbd0f6fa8e294f5ce7d362e9
falana sheriff <falanasheriffdeen@gmail.com>
2023-07-02 13:39:39 +0100

    work on nuxtjs

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="navbar">
+            <p>Kanipe this is the navbar.</p>
+        </div>
+        <div>
+            <slot />
+        </div>
+        <div class="footer">
+            <h1></h1>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.navbar {
+    width: 100%;
+    height: 80px;
+    border: 2px solid red;
+    background: powderblue;
+}
+.footer {
+    width: 100%;
+    height: 500px;
+    border: 2px solid red;
+    background: lemonchiffon;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>WELCOME TO THIS APPLICATION.</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+* {
+    box-sizing: border-box;
+}
+body, html {
+    margin: 0px !important;
+    padding: 0px !important;    
+}
+</style>
