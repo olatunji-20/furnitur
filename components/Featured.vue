6d23532d39034025e313a383a73b292801109e8e
falana sheriff <falanasheriffdeen@gmail.com>
2023-07-05 14:09:10 +0100

    created featured component

---
+++ components/Featured.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="border-8 border-green-900 w-[100%] h-[600px] text-center p-16">
+        <p class="font-bold text-6xl">FEATURED</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Featured'
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="">
-    <section class="border-4 border-red-900 w-[100%] h-[600px] bg-blue-100">
-      <div class="border-2 border-green-700 w-[600px] h-[400px]"><p>FURNITURE GALLERY</p></div>
+    <section style="background-image: url(../asssets/hero.png);" class="border-4 border-red-900 w-[100%] h-[500px] bg-cover">
+      <div class="border-2 border-green-700 w-[600px] h-[100%] float-right">
+        <h1 class="text-6xl font-bold p-16">FURNITURE GALLERY</h1>
+      </div>
     </section>
     <Reviews />
     <Trusted />
+    <Featured />
     <Choose />
     <h1>WELCOME TO THIS APPLICATION.</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla sunt, iste laborum maxime ad veritatis, autem tenetur eligendi corporis voluptas accusantium a impedit quibusdam 
@@ -18,8 +21,10 @@
 import Choose from '~/components/Choose'
 import Trusted from '~/components/Trusted'
 import Reviews from '~/components/Reviews'
+import Featured from '~/components/Featured'
+
   export default {
-    components: { Choose, Trusted, Reviews }
+    components: { Choose, Trusted, Reviews, Featured }
   }
 </script>
 
