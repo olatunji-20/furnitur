acdce95875fd315e55d30bbe9b9db722b0e9b973
falana sheriff <falanasheriffdeen@gmail.com>
2023-09-02 10:10:10 +0100

    continue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt'],
-  pinia: {
-    autoImports: [
-      // automatically imports `defineStore`
-      'defineStore', // import { defineStore } from 'pinia'
-      ['defineStore', 'definePiniaStore']
-    ]
-  }
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
+  
 })
