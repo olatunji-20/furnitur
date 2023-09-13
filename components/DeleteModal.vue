65c8d101a8b51acced7939ab463f458af4dfd2b6
falana sheriff <falanasheriffdeen@gmail.com>
2023-09-13 17:09:21 +0100

    little work on furnitur

--- components/DeleteModal.vue
+++ components/DeleteModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border-4 border-green-300 bg-gray-200 fixed top-0 right-0 bottom-0 left-0 z-50 flex justify-center items-center">
+  <div class="border-4 border-green-300 bg-gray-50 fixed top-0 right-0 bottom-0 left-0 z-50 flex justify-center items-center">
     <div class="border-2 border-red-400 w-[500px] h-[400px] rounded-md bg-blue-300 text-center pt-12">
         <h4>Do you want to remove this item from your cart?</h4>
         <div class="border-2 border-green-400 w-[400px] mx-auto my-40 flex justify-between">
